5ba8c2ae43a7df33182fb09b4d61bd4876ae103e
SajalSood <sajal.sood@gmail.com>
2020-12-29 12:58:43 -0500

    more fix

--- scripts/applicationstart.sh
+++ scripts/applicationstart.sh
@@ -8,6 +8,7 @@ echo "Description=Portfolio Service" >> /etc/systemd/system/portfolio.service
 echo "After=network.target" >> /etc/systemd/system/portfolio.service
 echo "[Service]" >> /etc/systemd/system/portfolio.service
 echo "User=root" >> /etc/systemd/system/portfolio.service
+echo "Environment=PORT=80" >> /etc/systemd/system/portfolio.service
 echo "WorkingDirectory=/var/www/portfolio/" >> /etc/systemd/system/portfolio.service
 echo "ExecStart=/var/www/portfolio/_build/dev/rel/personal_site/bin/personal_site start" >> /etc/systemd/system/portfolio.service
 echo "[Install]" >> /etc/systemd/system/portfolio.service
--- scripts/bootstrap_ec2.sh
+++ scripts/bootstrap_ec2.sh
@@ -5,12 +5,12 @@ sudo wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && 
 sudo apt-get update
 sudo apt-get install esl-erlang -y
 sudo apt-get install elixir -y
+cd /var/www/portfolio/
 yes | mix local.hex
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install v13
-cd /var/www/portfolio/
 sudo mix deps.get
-yes Y | sudo mix compile
+yes yes s| sudo mix compile
 sudo mix release.init
 sudo mix release
